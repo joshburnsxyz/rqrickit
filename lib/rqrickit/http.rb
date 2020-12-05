86e32e2ccc3443b906cad357c68347dfdf3ae171
Josh Burns <josh@tripwyre.me>
2020-12-05 23:35:10 +1100

    remove test as its popping a false positive for a failed test

--- lib/rqrickit/http.rb
+++ lib/rqrickit/http.rb
@@ -1,19 +1,22 @@
 require 'net/http'
+require 'erb'
 
 module Rqrickit
   module Http
 
     class Client
-      def initialize(base_url)
-        @base_url = base_url
+      def initialize(last_name, first_name, company_name, job_position, email, phone, website)
+        @last_name = ERB::Util.url_encode(last_name)
+        @first_name = ERB::Util.url_encode(first_name)
+        @company_name = ERB::Util.url_encode(company_name)
+        @job_position = ERB::Util.url_encode(job_position)
+        @email = ERB::Util.url_encode(email)
+        @phone = ERB::Util.url_encode(phone)
+        @website = ERB::Util.url_encode(website)
       end
-
-      def get_vcard_qr_code(last_name, first_name, company_name, job_position, email, phone, website, address)
+      def get_vcard_qr_code
         notes = "Created by Contactify"
-        api_string = "https://qrickit.com/api/qr.php?d=BEGIN%3aVCARD%0d%0aVERSION%3a3.0%0d%0aN%3a#{ERB::Util.url_encode(last_name)}%3b#{ERB::Util.url_encode(first_name)}%0d%0aORG
-%3a#{ERB::Util.url_encode(company_name)}%0d%0aTITLE%3a#{ERB::Util.url_encode(job_position)}%0d%0aEMAIL%3a#{ERB::Util.url_encode(email)}%0d%0aTEL%3bTYPE%3d
-CELL%3a#{ERB::Util.url_encode(phone)}%0d%0aTEL%3bTYPE%3dWORK%2cVOICE%3a%0d%0aNOTE%3a#{ERB::Uil.url_encode(notes)}%0d%0a
-URL%3a#{website}%0d%0aADR%3a%3b%3b%0d%0aEND%3aVCARD%0A&addtext=&txtcolor=000000&fgdcolor=000000&bgdcolor=FFFFFF&qrsize=200"
+        "https://qrickit.com/api/qr.php?d=BEGIN%3aVCARD%0d%0aVERSION%3a3.0%0d%0aN%3a#{@last_name}%3b#{@first_name}%0d%0aORG%3a#{@company_name}%0d%0aTITLE%3a#{@job_position}%0d%0aEMAIL%3a#{@email}%0d%0aTEL%3bTYPE%3dCELL%3a#{@phone}%0d%0aTEL%3bTYPE%3dWORK%2cVOICE%3a%0d%0aNOTE%3a#{notes}%0d%0aURL%3a#{@website}%0d%0aADR%3a%3b%3b%0d%0aEND%3aVCARD%0A&addtext=&txtcolor=000000&fgdcolor=000000&bgdcolor=FFFFFF&qrsize=200"
       end
     end
 
